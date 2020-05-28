6e87584209a98955ba8a4089fa31a6d03c8d8a01
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-05-28 19:57:07 +0100

    Add pflag so we can take bot token as an arg

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dshoreman/smegbot
 
 go 1.14
+
+require github.com/ogier/pflag v0.0.1
--- smegbot.go
+++ smegbot.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	flag "github.com/ogier/pflag"
+)
+
+var (
+	token string
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "\nOptions:\n\n")
+		flag.PrintDefaults()
+	}
+
+	flag.StringVarP(&token, "token", "t", "", "Bot Token")
+	flag.Parse()
+}
 
 func main() {
 	fmt.Println("8\"\"\"\"8                    8\"\"\"\"8               ")
@@ -9,4 +29,11 @@ func main() {
 	fmt.Println("    88 8  8  8 8ee  8     88     8 8   8   8   ")
 	fmt.Println("e   88 8  8  8 8    8  \"8 88     8 8   8   8   ")
 	fmt.Println("8eee88 8  8  8 88ee 88ee8 88eeeee8 8eee8   8   ")
+
+	fmt.Println("\nInitialising...")
+
+	if token == "" {
+		fmt.Println("Error: Token must be set. Aborting.")
+		os.Exit(1)
+	}
 }
