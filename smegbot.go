b5bbe3d9d48890c1223deb377337ea3a053599ec
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-05-29 16:57:07 +0100

    Add .nuke command for adding members to quarantine

--- smegbot.go
+++ smegbot.go
@@ -117,6 +117,31 @@ func onMessageReceived(s *discord.Session, m *discord.MessageCreate) {
 		return
 	}
 
+	if len(m.Content) > 6 && m.Content[0:6] == ".nuke " {
+		sinbin := ""
+		guildRoles, _ := s.GuildRoles(m.GuildID)
+		for _, role := range guildRoles {
+			if role.Name == "Quarantine" {
+				sinbin = role.ID
+			}
+		}
+		if sinbin == "" {
+			s.ChannelMessageSend(m.ChannelID, "I couldn't find the **@Quarantine** role!")
+			return
+		}
+
+		target := m.Mentions[0].ID
+		err := s.GuildMemberRoleAdd(m.GuildID, target, sinbin)
+		if err != nil {
+			s.ChannelMessageSend(m.ChannelID, "Something went wrong trying to add the **@Quarantine** role.")
+			fmt.Println("\nError: Could not add Quarantine role.\n", err)
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Member @%s is now in Quarantine!", target))
+		return
+	}
+
 	if m.Content == "ping" {
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
 		return
