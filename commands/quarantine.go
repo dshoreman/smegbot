1b4d1b58ddcdbdde074d1967ecea9b83ebdeebbf
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-05-31 16:04:15 +0100

    Save and remove member roles when quarantining
    This stores a JSON representation of the nukee's roles to `roles.json`
    in `storage/guilds/<guildID>/<memberID>` before removing each assigned
    role once the quarantine role has been added.

--- commands/quarantine.go
+++ commands/quarantine.go
@@ -1,7 +1,11 @@
 package commands
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	discord "github.com/bwmarrin/discordgo"
 )
@@ -10,7 +14,16 @@ func nuke(s *discord.Session, m *discord.MessageCreate) {
 	sinbin := quarantineRole(s, m.ChannelID, m.GuildID)
 	if sinbin != "" {
 		target := m.Mentions[0].ID
-		err := s.GuildMemberRoleAdd(m.GuildID, target, sinbin)
+		roles := memberRoles(s, m.GuildID, target)
+
+		saved, err := backupRoles(s, m.ChannelID, m.GuildID, target, roles)
+		if err != nil {
+			fmt.Println("\nError: Could not save roles to file\n", err)
+		}
+		err = s.GuildMemberRoleAdd(m.GuildID, target, sinbin)
+		if saved && err == nil {
+			removeRoles(s, m.GuildID, target, roles)
+		}
 		sendSuccessOrFail(s, m.ChannelID, err, "add", target)
 	}
 }
@@ -34,6 +47,35 @@ func quarantineRole(s *discord.Session, channelID string, guildID string) string
 	}
 	s.ChannelMessageSend(channelID, "I couldn't find the **@Quarantine** role!")
 	return ""
+}
+
+func memberRoles(s *discord.Session, guildID string, target string) []string {
+	member, err := s.GuildMember(guildID, target)
+	roles := make([]string, 0)
+	if err == nil {
+		roles = append(roles, member.Roles...)
+	}
+	return roles
+}
+
+func backupRoles(s *discord.Session, channelID string, guildID string, target string, roles []string) (bool, error) {
+	b, err := json.Marshal(roles)
+	if err != nil {
+		return false, err
+	}
+	path := filepath.Join("./storage/guilds", guildID, "members", target)
+	err = os.MkdirAll(path, 0700)
+	if err == nil {
+		err = ioutil.WriteFile(filepath.Join(path, "roles.json"), b, 0644)
+	}
+	return err == nil, err
+}
+
+func removeRoles(s *discord.Session, guildID string, userID string, roles []string) {
+	for _, role := range roles {
+		fmt.Printf("Removing role %s...\n", role)
+		s.GuildMemberRoleRemove(guildID, userID, role)
+	}
 }
 
 func sendSuccessOrFail(s *discord.Session, channelID string, err error, mode string, target string) {
