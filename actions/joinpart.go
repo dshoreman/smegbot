bf16a938165106ebebe73316559942a1ad2fbfbf
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-06-03 05:45:06 +0100

    Listen for member update events
    When a member is updated we spit out their @username#discriminator in
    Discord along with the new nick. This is mostly PoC and will be gone
    once we're writing the nick to a file in storage instead.

--- actions/joinpart.go
+++ actions/joinpart.go
@@ -10,6 +10,10 @@ func onJoin(s *dg.Session, m *dg.GuildMemberAdd) {
 	s.ChannelMessageSend(getChannel(s, m.GuildID), fmt.Sprintf("<@%s> has joined! :wave:", m.User.ID))
 }
 
+func onChange(s *dg.Session, m *dg.GuildMemberUpdate) {
+	s.ChannelMessageSend(getChannel(s, m.GuildID), fmt.Sprintf("@%s#%s has a new nick: %s", m.User.Username, m.User.Discriminator, m.Nick))
+}
+
 func onPart(s *dg.Session, m *dg.GuildMemberRemove) {
 	s.ChannelMessageSend(getChannel(s, m.GuildID), fmt.Sprintf("<@%s> has left :slight_frown:", m.User.ID))
 }
--- actions/main.go
+++ actions/main.go
@@ -9,6 +9,9 @@ func Register(dg *discord.Session) {
 	dg.AddHandler(func(s *discord.Session, m *discord.GuildMemberAdd) {
 		onJoin(s, m)
 	})
+	dg.AddHandler(func(s *discord.Session, m *discord.GuildMemberUpdate) {
+		onChange(s, m)
+	})
 	dg.AddHandler(func(s *discord.Session, m *discord.GuildMemberRemove) {
 		onPart(s, m)
 	})
