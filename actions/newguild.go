4a7704ecedb8eb0a099fc77e73941846de41ff43
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-06-12 10:26:02 +0100

    Add number of members with a nick/name saved

--- actions/newguild.go
+++ actions/newguild.go
@@ -14,11 +14,12 @@ func onGuildJoin(s *dg.Session, m *dg.GuildCreate) {
 func printGuildInfo(s *dg.Session, g *dg.Guild) {
 	joinDate, _ := g.JoinedAt.Parse()
 	f, output := "15:04:05 on January 2, 2006", `Smegbot has joined %s! %s
-   Smegbot first joined the guild at %s. It currently has %d members.
+   Smegbot first joined at %s. Of %d members, %d have had names saved.
    Guild is owned by <@%s>. System messages are sent to <#%s>.
+
 `
 	fmt.Printf(output, g.Name, hasConfig(g),
-		joinDate.Format(f), g.MemberCount,
+		joinDate.Format(f), g.MemberCount, withNames(g),
 		g.OwnerID, g.SystemChannelID)
 }
 
@@ -27,4 +28,15 @@ func hasConfig(g *dg.Guild) string {
 		return "It appears to have config already."
 	}
 	return "Seems it's missing its config too!"
+}
+
+func withNames(g *dg.Guild) int {
+	count := 0
+	for _, m := range g.Members {
+		f := util.GuildPath("m.nick", g.ID, m.User.ID)
+		if util.FileExists(f) && util.ReadString(f) != "" {
+			count++
+		}
+	}
+	return count
 }
