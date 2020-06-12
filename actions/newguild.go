08c2d07c8c3f8f1e4bd614bfe93cf34341a5c9fd
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-06-12 10:01:12 +0100

    Show whether guild has config saved in join output

--- actions/newguild.go
+++ actions/newguild.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	dg "github.com/bwmarrin/discordgo"
+	"github.com/dshoreman/smegbot/util"
 )
 
 func onGuildJoin(s *dg.Session, m *dg.GuildCreate) {
@@ -12,9 +13,18 @@ func onGuildJoin(s *dg.Session, m *dg.GuildCreate) {
 
 func printGuildInfo(s *dg.Session, g *dg.Guild) {
 	joinDate, _ := g.JoinedAt.Parse()
-	f, output := "15:04:05 on 2nd January 2006", `Smegbot has joined %s!
-   Server has %d members and is owned by <@%s>.
-   Smegbot joined at %s. System messages are sent to <#%s>.
+	f, output := "15:04:05 on January 2, 2006", `Smegbot has joined %s! %s
+   Smegbot first joined the guild at %s. It currently has %d members.
+   Guild is owned by <@%s>. System messages are sent to <#%s>.
 `
-	fmt.Printf(output, g.Name, g.MemberCount, g.OwnerID, joinDate.Format(f), g.SystemChannelID)
+	fmt.Printf(output, g.Name, hasConfig(g),
+		joinDate.Format(f), g.MemberCount,
+		g.OwnerID, g.SystemChannelID)
+}
+
+func hasConfig(g *dg.Guild) string {
+	if util.FileExists(util.GuildPath("config", g.ID)) {
+		return "It appears to have config already."
+	}
+	return "Seems it's missing its config too!"
 }
