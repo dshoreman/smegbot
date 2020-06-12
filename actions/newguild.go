cae7af08ac4094d2c8531e0fab3f980f85ead9b2
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-06-12 09:28:00 +0100

    Add members, owner, joindate and system channel

--- actions/newguild.go
+++ actions/newguild.go
@@ -7,5 +7,14 @@ import (
 )
 
 func onGuildJoin(s *dg.Session, m *dg.GuildCreate) {
-	fmt.Println("Smegbot has joined", m.Guild.Name)
+	printGuildInfo(s, m.Guild)
+}
+
+func printGuildInfo(s *dg.Session, g *dg.Guild) {
+	joinDate, _ := g.JoinedAt.Parse()
+	f, output := "15:04:05 on 2nd January 2006", `Smegbot has joined %s!
+   Server has %d members and is owned by <@%s>.
+   Smegbot joined at %s. System messages are sent to <#%s>.
+`
+	fmt.Printf(output, g.Name, g.MemberCount, g.OwnerID, joinDate.Format(f), g.SystemChannelID)
 }
